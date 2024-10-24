0f3f8663a0ae65c6e836da84a0d579893a708616
wunai <1751660300@qq.com>
2024-10-24 23:52:53 +0800

    Changes：解决无法将所有属性配置到标签内的问题

--- flexx/ui/_widget.py
+++ flexx/ui/_widget.py
@@ -495,11 +495,12 @@ class Widget(app.JsComponent):
         map = {'css_class': 'className', 'class': 'className'}
         for key, val in vnode.props.items():
             ob = node
-            parts = key.replace('__', '.').split('.')
-            for i in range(len(parts)-1):
-                ob = ob[parts[i]]
-            key = parts[len(parts)-1]
-            ob[map.get(key, key)] = val
+            ob.setAttribute(key, val)
+            # parts = key.replace('__', '.').split('.')
+            # for i in range(len(parts)-1):
+            #     ob = ob[parts[i]]
+            # key = parts[len(parts)-1]
+            # ob[map.get(key, key)] = val
 
         # Resolve content
         if vnode.children is None:
